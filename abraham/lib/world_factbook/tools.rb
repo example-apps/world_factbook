aa2a9068ba9e2d7b2b3caa72a056e9cad4541323
Abraham Oshel <abraham.oshel@gmail.com>
2013-04-19 15:48:22 -0500

    add country listing

--- abraham/lib/world_factbook/tools.rb
+++ abraham/lib/world_factbook/tools.rb
@@ -8,6 +8,19 @@ module WorldFactbook
       (match.send(fact).to_f / 100)
     end
 
+    def list_countries(order=:smallest)
+      get_data
+      grouped_countries = @countries.group_by {|c| c.continent }
+      grouped_countries.each do |k , v|
+        puts "In #{k} there is:"
+        sorted = order == :smallest ? v.sort_by {|country| country.name } : v.sort_by {|country| country.name }.reverse
+        sorted.each do |country|
+          puts "--- #{country.name}"
+        end
+      end
+      return
+    end
+
     def print_result(matches, fact)
       matches.each do |m|
         puts "#{m.name} - with #{fact.capitalize} of #{convert_data(m, fact)}"
@@ -19,12 +32,12 @@ module WorldFactbook
     end
 
     def method_missing(method_id, *arguments, &block)
-      if [:least, :greatest].include?(method_id)
+      if [:smallest, :greatest].include?(method_id)
         get_data
         fact = arguments.first
         limit = arguments.last
         matches = countries_cleaned(@countries, fact).sort_by { |c| c.send(fact) }
-        sorted_reults = method_id == :least ? matches.take(limit) : matches.reverse.take(limit)
+        sorted_reults = method_id == :smallest ? matches.take(limit) : matches.reverse.take(limit)
         print_result(sorted_reults, fact)
         return
       else
