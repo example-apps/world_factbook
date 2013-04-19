9edb1b60556168011979616dd386869a96d1b5ff
Abraham Oshel <abraham.oshel@gmail.com>
2013-04-19 15:27:45 -0500

    clean up comments and commented out code

--- abraham/lib/world_factbook/tools.rb
+++ abraham/lib/world_factbook/tools.rb
@@ -1,19 +1,5 @@
 module WorldFactbook
   class << self
-    # def greatest(fact, limit=1)
-    #   get_data
-    #   matches = countries_cleaned(@countries, fact).sort_by { |c| c.send(fact) }.reverse.take(limit)
-    #   print_result(matches, fact)
-    #   return
-    # end
-
-    # def least(fact, limit=1)
-    #   get_data
-    #   matches = countries_cleaned(@countries, fact).sort_by { |c| c.send(fact) }.take(limit)
-    #   print_result(matches, fact)
-    #   return
-    # end
-
     def get_data
       @countries ||= Country.create_countries
     end
@@ -47,13 +33,3 @@ module WorldFactbook
     end
   end
 end
-
-
-  # max_pop = doc.css("country").collect {|c| c['population'].to_i}.max
-  # doc.search("country[population='#{max_pop}']")
-
-  # inflation = doc.search('country').sort {|lt, gt| gt['inflation'].to_i <=> lt['inflation'].to_i }
-  # inflation.take(5).collect {|c| c['name'] + c['inflation']}
-  # hash = {}
-  # continents = doc.css('continent').map {|c| hash[c['name']] = []}
-  # doc.search('country').each {|c| hash[c['continent']] << c['name']}
